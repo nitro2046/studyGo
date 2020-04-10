32bbf77e011d8a691d942295018dfb175a66b817
nitro <xiaoren8688@yahoo.co.jp>
2020-04-11 01:15:33 +0900

    complate

--- 11-cond.go
+++ 11-cond.go
@@ -1,28 +1,71 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 var wg sync.WaitGroup
 var mtex sync.Mutex
+var con *sync.Cond
+var shop [5]int
+
+var conMaker *sync.Cond
+var mtexMaker sync.Mutex
+var mianbaoCount int = 0
+
+var eatNum int = 0
 
 func main() {
-	for {
-		wg.Add(1)
-		go makePan()
-	}
-
-	for {
-		wg.Add(1)
-		go eatPan()
-	}
+	con = sync.NewCond(&mtex)
+	conMaker = sync.NewCond(&mtexMaker)
+	wg.Add(2)
+	go makePan()
+	go eatPan(1)
+	go eatPan(2)
+	go eatPan(3)
 
 	wg.Wait()
 }
 
 func makePan() {
+	mianbao := 1001
+	num := 0
+	for {
+		mtex.Lock()
+		time.Sleep(time.Millisecond * 100)
+		shop[num] = mianbao
+		fmt.Printf("生产了%d号饼\n", mianbao)
+		mianbao++
+		mianbaoCount++
+		num = (num + 1) % 5
+		con.Signal()
+		mtex.Unlock()
+
+		if mianbaoCount == 5 {
+			mtexMaker.Lock()
+			conMaker.Wait()
+			mtexMaker.Unlock()
+		}
+	}
+
 	wg.Done()
 }
 
-func eatPan() {
+func eatPan(p int) {
+	for {
+		mtex.Lock()
+		con.Wait()
+		if mianbaoCount > 0 {
+			time.Sleep(time.Millisecond * 10)
+			fmt.Printf("%d号吃货，吃了%d号饼\n", p, shop[eatNum])
+			eatNum = (eatNum + 1) % 5
+			mianbaoCount--
+		}
+		mtex.Unlock()
+		conMaker.Signal()
+
+	}
 	wg.Done()
 }
